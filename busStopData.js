070ed8658033501613347d6a5554c8a99ef0e55f
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-01-20 10:05:17 +0000

    Clean up code

--- busStopData.js
+++ busStopData.js
@@ -1,5 +1,5 @@
 export async function busStopData(ourcoordinates) {
-    let url = `https://api.tfl.gov.uk/StopPoint/?lat=${ourcoordinates.latitude}&lon=${ourcoordinates.longitude}&stopTypes=NaptanPublicBusCoachTram`
+    let url = `https://api.tfl.gov.uk/StopPoint/?lat=${ourcoordinates.latitude}&lon=${ourcoordinates.longitude}&stopTypes=NaptanPublicBusCoachTram`;
     let response = await fetch(url);
     let busData = await response.json();
 
--- coordinates.js
+++ coordinates.js
@@ -4,15 +4,15 @@ import { postcodeRequest } from "./postcodeRequest.js";
 export async function coordinates() {
 
     let postcode = postcodeRequest('What is your postcode? ');
-    let url = "https://api.postcodes.io/postcodes/" + postcode
+    let url = "https://api.postcodes.io/postcodes/" + postcode;
     let response = await fetch(url);
-    let coordinates = await response.json()
+    let coordinates = await response.json();
 
     while (!coordinates.result) {
         postcode = postcodeRequest("Please enter a valid postcode: ");
-        url = "https://api.postcodes.io/postcodes/" + postcode
+        url = "https://api.postcodes.io/postcodes/" + postcode;
         response = await fetch(url);
-        coordinates = await response.json()
+        coordinates = await response.json();
     }
 
     return { longitude: coordinates.result.longitude, latitude: coordinates.result.latitude };
